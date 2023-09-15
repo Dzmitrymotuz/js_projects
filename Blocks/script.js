3057cc07ca1ec85aa7243f56068d302652af79e4
dzmitrymotuz <gockie51>
2023-09-15 11:14:42 -0400

    Blocks

---
+++ Blocks/script.js
@@ -0,0 +1,9 @@
+document.getElementsByClassName("fa-brands fa-youtube")[2].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=Rzzt-s4pZjY&t=82s");
+})
+document.getElementsByClassName("fa-brands fa-youtube")[1].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=Rzzt-s4pZjY&t=82s");
+})
+document.getElementsByClassName("fa-brands fa-youtube")[2].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=Rzzt-s4pZjY&t=82s");
+})
