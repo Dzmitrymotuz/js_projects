57ad3d544763de431af03c3ed02b7d69c92cbbb8
dzmitrymotuz <gockie51>
2023-10-27 11:39:30 -0400

    Routing

--- bisapp/src/App.js
+++ bisapp/src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from "./Home.js"
+import './App.css';
 import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
 import { Navbar } from './components';
-import './App.css';
 
 const App = () => (
-  <div>
-    <Navbar />
-    <Header />
-    <AboutUs />
-    <SpecialMenu />
-    <Chef />
-    <Intro />
-    <Laurels />
-    <Gallery />
-    <FindUs />
-    <Footer />
-  </div>
+    <Router>
+      {/* <Navbar/> */}
+      <Routes>
+        <Route path='/' element={<Home/>}>Hello!</Route>
+        <Route path='/home' element={<AboutUs/>}>Hello!</Route>
+      </Routes>
+    </Router>
 );
 
 export default App;
